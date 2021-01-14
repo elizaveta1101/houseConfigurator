89008ebfd4b1f0e3ad314a25b084cecad4e6a9da
Aleksandr Borisov <alexandr.keeper@yandex.ru>
2021-01-14 20:47:55 +0300

    update repo

--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,30 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter, Router } from 'react-router-dom'
+import { BrowserRouter, Redirect, Route, Router, Switch } from 'react-router-dom'
 import { createHashHistory, History } from 'history'
 import { Provider } from 'react-redux'
 
 import store from './store'
+import ClientSideStore from './ClientSide/redux/store'
 
-import App from './App'
+import ControlPanel from './App'
+import ClientSide from './ClientSide/App'
 
 const history: History = createHashHistory()
 
 ReactDOM.render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <Router history={history}>
-        <App />
-      </Router>
-    </BrowserRouter>
-  </Provider>,
+  <BrowserRouter>
+    <Router history={history}>
+      <Switch>
+        {/* <Provider store={store}>
+          <Route path={'/admin'} component={ControlPanel} />
+        </Provider> */}
+        <Provider store={ClientSideStore}>
+          <Route path={'/'} component={ClientSide} />
+        </Provider>
+        <Redirect to={'/'} />
+      </Switch>
+    </Router>
+  </BrowserRouter>,
   document.getElementById('root')
 )
